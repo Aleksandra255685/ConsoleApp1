19f30719736811e39f516c59ded2244e4d1c90c2
Александра Венатовская <sashaven2003@mail.ru>
2021-12-16 17:16:52 +0700

    Доделала какой-то новой херью.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,40 +9,68 @@ namespace ConsoleApp1
             int n = 10;
             double[] a = new double[n];
             double[] b = new double[n];
-            Console.WriteLine("Введите массив A");
+            Random rand = new Random();
+            //создаем и сортируем массив А
             for (int i = 0; i < n; i++)
             {
-                a[i] = Convert.ToDouble(Console.ReadLine()); // считываем элемент
+                a[i] = rand.Next(-100, 100);
             }
-            Console.WriteLine("Введите массив B");
+            Array.Sort(a);
+            Console.WriteLine("Массив A:");
             for (int i = 0; i < n; i++)
             {
-                b[i] = Convert.ToDouble(Console.ReadLine()); // считываем элемент
+                Console.Write(a[i] + " ");
             }
+            Console.WriteLine();
+            //создаем и сортируем массив B
+            for (int i = 0; i < n; i++)
+            {
+                b[i] = rand.Next(-100, 100);
+            }
+            Array.Sort(b);
+            Console.WriteLine("Массив B:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(b[i] + " ");
+            }
+            Console.WriteLine();
 
-            double[] A = new double[n * 2];
-            int l = 0, j = 0, k = 0;
-            while (l < n && j < n)
+            //метод слияния
+            //пока не достигнем конца одного из массивов, выбираем меньший из элементов и добавляем в итоговый массив
+            double[] A = new double[2*n];
+            int l = 0, m = 0, k = 0;
+            while (l < n && m < n)
             {
-                if (a[l] <= b[j])
+                if (a[l] <= b[m])
                 {
                     A[k] = a[l];
                     l++;
                 }
                 else
                 {
-                    A[k] = b[j];
-                    j++;
+                    A[k] = b[m];
+                    m++;
                 }
                 k++;
             }
-            Console.WriteLine("rfr ltkf");
-
-            Console.WriteLine("Вывод A: ");
-            for (int i = 0; i < A.Length; i++)
+            //когда один из массивов заканчивается, записываем в итоговый массив оставшиеся элементы
+            while (l < n)
+            {
+                A[k] = a[l];
+                l++;
+                k++;
+            }
+             while (m < n)
+             {
+                A[k] = b[m];
+                m++;
+                k++;
+             }
+           
+            Console.WriteLine("Полученный массив: ");
+            for (int i = 0; i < 2*n; i++)
             {
                 Console.Write(A[i] + " ");
-
             }
         }
     }
